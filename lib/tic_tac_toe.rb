1cbe548dc43806ce5e3cfe2809d114e24a279a11
Sarah Harrs <sarahharrs@gmail.com>
2019-07-17 12:11:18 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,15 @@
 class TicTacToe
+  WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],  # Bottom row
+    [0,3,6],  # Left col
+    [1,4,7],  # Middle col
+    [2,5,8],  # Right col
+    [0,4,8],  # Diag
+    [2,4,6]  # Diag
+  ]
+
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
@@ -18,4 +29,39 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+def move
+end
+
+def position_taken?
+end
+
+def valid_move?
+end
+
+def turn
+end
+
+def turn.count
+end
+
+def won?
+end
+
+def full?
+end
+
+def draw?
+end
+
+def over?
+end
+
+def winner
+end
+
+def play
+end
+
+
 end
