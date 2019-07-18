44479074fe4a897fa706a3300eba50ffc7c815a6
Sarah Harrs <sarahharrs@gmail.com>
2019-07-18 00:35:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,6 +62,16 @@ def over?
 end
 
 def winner
+  WIN_COMBINATIONS.detect do |combo|
+          if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+            return "X"
+          elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+            return "O"
+          else
+            nil
+          end
+    end
+  end
 end
 
 def play
